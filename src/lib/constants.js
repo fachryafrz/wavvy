8968b47f9ead3fd212b2afd4215c8165031425fb
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-09-14 18:08:20 +0700

    update: GA4 Measurement ID

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-export const GA_MEASUREMENT_ID = "G-L0V4DXC6HK";
+export const GA_MEASUREMENT_ID = "G-T493HCG26P";
 export const SPOTIFY_ACCESS_TOKEN = "spotify.access_token";
 export const SPOTIFY_AUTHORIZATION = "spotify.authorization";
 export const SPOTIFY_REFRESH_TOKEN = "spotify.refresh_token";
