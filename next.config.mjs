f856dce1527848431925e94b1d6aa4294cf7c876
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-10-26 23:29:53 +0700

    remove: staletimes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
-    staleTimes: {
-      dynamic: 1800, // 30 minutes in seconds
-    },
-  },
 };
 
 export default nextConfig;
