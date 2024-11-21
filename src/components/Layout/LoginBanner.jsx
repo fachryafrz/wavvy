9edc41bcea610721843ead88f6e5807cd60cc51a
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2024-11-21 13:36:41 +0700

    add: image to login alert

--- src/components/Layout/LoginBanner.jsx
+++ src/components/Layout/LoginBanner.jsx
@@ -7,10 +7,10 @@ export default function LoginBanner({ authorizationURL, client_id }) {
 
   return (
     <div
-      className={`rounded-xl bg-gradient-to-tl from-[#3E2532] via-[#24202B] via-60% to-[#24202B] p-6 @container`}
+      className={`relative overflow-hidden rounded-xl bg-gradient-to-tl from-[#3E2532] via-[#24202B] via-60% to-[#24202B] p-6 @container`}
     >
       <div
-        className={`flex flex-col justify-between gap-6 @lg:flex-row @lg:items-center`}
+        className={`relative z-10 flex flex-col justify-between gap-6 @lg:flex-row @lg:items-center`}
       >
         <div className={`flex flex-col gap-2`}>
           <h2 className={`text-pretty text-xl font-medium @sm:text-2xl`}>
@@ -33,7 +33,16 @@ export default function LoginBanner({ authorizationURL, client_id }) {
       </div>
 
       {/* Image of Ryth when Logged In (Coming Soon) */}
-      {/* <figure></figure> */}
+      <figure
+        className={`-mx-6 -mb-6 @sm:m-0 flex items-end justify-end overflow-hidden after:absolute after:inset-0 after:hidden after:bg-gradient-to-tl after:from-transparent after:via-[#24202B] after:to-[#24202B] @sm:absolute @sm:inset-0 @sm:after:block`}
+      >
+        <img
+          src="/screenshots/ryth-diagonal-720.png"
+          draggable={false}
+          alt=""
+          className={`translate-x-[7px] translate-y-[15px] scale-150 @sm:w-[50%] @sm:-translate-x-[2px] @sm:translate-y-[18px]`}
+        />
+      </figure>
     </div>
   );
 }
