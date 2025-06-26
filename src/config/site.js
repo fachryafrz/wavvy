d1b94bca42c228cb0032410de19f2c233a6e1276
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-26 10:35:38 +0700

    chore: update site.js

--- src/config/site.js
+++ src/config/site.js
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Wavvy",
   description:
-    "Wavvy is a music streaming platform that offers seamless access to an extensive music library. You can explore detailed information about songs, albums, artists, and playlists. The intuitive search functionality makes it easy to find songs, artists, albums, and playlists effortlessly.",
+    "Music streaming platform that offers seamless access to an extensive music library. You can explore detailed information about songs, albums, artists, and playlists. The intuitive search functionality makes it easy to find songs, artists, albums, and playlists effortlessly.",
   url:
     process.env.NODE_ENV === "production"
       ? "https://wavvy.fachryafrz.com"
